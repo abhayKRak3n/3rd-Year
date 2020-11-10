3ce3203f8e5ddaf19d4037b109ea0be87a9111fa
Abhay Khanka <58368777+abhayKRak3n@users.noreply.github.com>
2020-11-10 16:47:26 +0000

    initial commit

---
+++ FunctionalProgramming/Ex00.hs
@@ -0,0 +1,21 @@
+module Ex00 where
+
+name, idno, username :: String
+name      =  "Khanka, Abhay"  -- replace with your name
+idno      =  "18309999"    -- replace with your student id
+username  =  "khankaa"  -- replace with your TCD username
+
+
+declaration -- do not modify this
+ = unlines
+     [ ""
+     , "@@@ This exercise is all my own work."
+     , "@@@ Signed: " ++ name
+     , "@@@ "++idno++" "++username
+     ]
+
+{- Modify everything below here as you see fit to ensure all tests pass -}
+
+hello  =  "Hello World :-)"
+
+
